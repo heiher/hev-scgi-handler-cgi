8ad8cd6ff8a8daeeacef926b320fce2a4c383100
heiher <admin@heiher.info>
2012-01-30 14:46:58 +0800

    Update

--- src/hev-scgi-handler-cgi.c
+++ src/hev-scgi-handler-cgi.c
@@ -16,13 +16,37 @@
 #include "hev-scgi-response.h"
 
 #define HEV_SCGI_HANDLER_CGI_NAME		"HevSCGIHandlerCGI"
-#define HEV_SCGI_HANDLER_CGI_VERSION	"0.0.3"
+#define HEV_SCGI_HANDLER_CGI_VERSION	"0.0.4"
 #define HEV_SCGI_HANDLER_CGI_PATTERN	"^.*\\.php($|\\?).*"
 
 #define HEV_SCGI_HANDLER_CGI_BIN_PATH	"/usr/bin/php-cgi"
 #define HEV_SCGI_HANDLER_CGI_WORK_DIR	"/tmp"
 
 #define BUFFER_SIZE                     4096
+
+typedef struct _HevSCGIHandlerCGITaskData HevSCGIHandlerCGITaskData;
+
+struct _HevSCGIHandlerCGITaskData
+{
+	GObject *scgi_request;
+	GObject *scgi_response;
+
+	GInputStream *req_input_stream;
+	GOutputStream *res_output_stream;
+	GInputStream *child_input_stream;
+	GOutputStream *child_output_stream;
+
+	GHashTable *req_hash_table;
+
+	gchar **envp;
+	guint envi;
+	gchar in_buffer[BUFFER_SIZE];
+	gchar out_buffer[BUFFER_SIZE];
+	guint in_count;
+	guint out_count;
+	guint content_len;
+	gboolean is_checked;
+};
 
 static void req_hash_table_foreach_handler(gpointer key,
 			gpointer value, gpointer user_data);
@@ -38,6 +62,7 @@ static void child_output_stream_close_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data);
 static void child_input_stream_close_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data);
+static void hev_scgi_handler_cgi_task_data_destroy_handler(gpointer data);
 
 G_MODULE_EXPORT gboolean hev_scgi_handler_module_init(HevSCGIHandler *self)
 {
@@ -70,88 +95,55 @@ G_MODULE_EXPORT const gchar * hev_scgi_handler_module_get_pattern(HevSCGIHandler
 
 G_MODULE_EXPORT void hev_scgi_handler_module_handle(HevSCGIHandler *self, GObject *scgi_task)
 {
-	GObject *scgi_request = NULL;
-	GObject *scgi_response = NULL;
-	GHashTable *req_hash_table = NULL;
-	GInputStream *input_stream = NULL;
-	gint content_length = 0;
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 	gchar *argv[] =
 	{
 		HEV_SCGI_HANDLER_CGI_BIN_PATH,
 		NULL
 	};
-	gchar **envp = NULL;
-	gint *envip = NULL;
 	gint in = 0, out = 0;
 	GPid pid = 0;
 	GError *error = NULL;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-	scgi_request = hev_scgi_task_get_request(HEV_SCGI_TASK(scgi_task));
-	scgi_response = hev_scgi_task_get_response(HEV_SCGI_TASK(scgi_task));
+	task_data = g_slice_new0(HevSCGIHandlerCGITaskData);
+	if(!task_data)
+	{
+		g_critical("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+		return;
+	}
+	g_object_set_data_full(scgi_task, "data", task_data,
+				hev_scgi_handler_cgi_task_data_destroy_handler);
 
-	req_hash_table = hev_scgi_request_get_header_hash_table(HEV_SCGI_REQUEST(scgi_request));
+	task_data->scgi_request = hev_scgi_task_get_request(HEV_SCGI_TASK(scgi_task));
+	task_data->scgi_response = hev_scgi_task_get_response(HEV_SCGI_TASK(scgi_task));
+	task_data->req_hash_table =
+		hev_scgi_request_get_header_hash_table(HEV_SCGI_REQUEST(task_data->scgi_request));
+	task_data->req_input_stream =
+		hev_scgi_request_get_input_stream(HEV_SCGI_REQUEST(task_data->scgi_request));
+	task_data->res_output_stream =
+		hev_scgi_response_get_output_stream(HEV_SCGI_RESPONSE(task_data->scgi_response));
+	task_data->content_len = atoi(g_hash_table_lookup(task_data->req_hash_table, "CONTENT_LENGTH"));
 
-	input_stream = hev_scgi_request_get_input_stream(HEV_SCGI_REQUEST(scgi_request));
+	task_data->envp =
+		g_malloc0_n(g_hash_table_size(task_data->req_hash_table)+1, sizeof(gchar *));
+	g_hash_table_foreach(task_data->req_hash_table, req_hash_table_foreach_handler, scgi_task);
 
-	content_length = atoi(g_hash_table_lookup(req_hash_table, "CONTENT_LENGTH"));
-
-	envp = g_malloc0_n(g_hash_table_size(req_hash_table)+1, sizeof(gchar *));
-	envip = g_malloc0(sizeof(gint));
-	g_object_set_data_full(scgi_task, "envp", envp,
-				(GDestroyNotify)g_strfreev);
-	g_object_set_data_full(scgi_task, "envip", envip, g_free);
-	g_hash_table_foreach(req_hash_table, req_hash_table_foreach_handler, scgi_task);
-
-	if(g_spawn_async_with_pipes(HEV_SCGI_HANDLER_CGI_WORK_DIR, argv, envp,
+	if(g_spawn_async_with_pipes(HEV_SCGI_HANDLER_CGI_WORK_DIR, argv, task_data->envp,
 					G_SPAWN_STDERR_TO_DEV_NULL, NULL, NULL,
 					&pid, &in, &out, NULL, &error))
 	{
-		GInputStream *child_input_stream = NULL;
-		GOutputStream *child_output_stream = NULL;
-		gchar *in_buffer = NULL;
-		gchar *out_buffer = NULL;
-		guint *in_count = NULL;
-		guint *out_count = NULL;
-		guint *content_len = NULL;
-		gboolean *is_checked = NULL;
+		task_data->child_input_stream = g_unix_input_stream_new(out, TRUE);
+		task_data->child_output_stream = g_unix_output_stream_new(in, TRUE);
+		task_data->is_checked = FALSE;
 
-		child_input_stream = g_unix_input_stream_new(out, TRUE);
-		child_output_stream = g_unix_output_stream_new(in, TRUE);
-		g_object_set_data(scgi_task, "child_input_stream",
-					child_input_stream);
-		g_object_set_data(scgi_task, "child_output_stream",
-					child_output_stream);
-
-		in_buffer = g_malloc0(BUFFER_SIZE);
-		g_object_set_data_full(scgi_task, "in_buffer",
-					in_buffer, g_free);
-		out_buffer = g_malloc0(BUFFER_SIZE);
-		g_object_set_data_full(scgi_task, "out_buffer",
-					out_buffer, g_free);
-		in_count = g_malloc0(sizeof(guint));
-		g_object_set_data_full(scgi_task, "in_count",
-					in_count, g_free);
-		out_count = g_malloc0(sizeof(guint));
-		g_object_set_data_full(scgi_task, "out_count",
-					out_count, g_free);
-		content_len = g_malloc0(sizeof(guint));
-		g_object_set_data_full(scgi_task, "content_len",
-					content_len, g_free);
-		is_checked = g_malloc0(sizeof(gboolean));
-		g_object_set_data_full(scgi_task, "is_checked",
-					is_checked, g_free);
-		*is_checked = FALSE;
-
-		*content_len = content_length;
-
-		g_input_stream_read_async(input_stream, in_buffer,
-					(content_length<BUFFER_SIZE)?content_length:BUFFER_SIZE,
+		g_input_stream_read_async(task_data->req_input_stream, task_data->in_buffer,
+					(task_data->content_len<BUFFER_SIZE)?task_data->content_len:BUFFER_SIZE,
 					G_PRIORITY_DEFAULT, NULL, req_input_stream_read_async_handler,
 					scgi_task);
 		g_object_ref(scgi_task);
-		g_input_stream_read_async(child_input_stream, out_buffer,
+		g_input_stream_read_async(task_data->child_input_stream, task_data->out_buffer,
 					BUFFER_SIZE, G_PRIORITY_DEFAULT, NULL,
 					child_input_stream_read_async_handler, scgi_task);
 		g_object_ref(scgi_task);
@@ -168,10 +160,10 @@ static void req_hash_table_foreach_handler(gpointer key,
 			gpointer value, gpointer user_data)
 {
 	GObject *scgi_task = user_data;
-	gchar **envp = g_object_get_data(scgi_task, "envp");
-	gint *envi = g_object_get_data(scgi_task, "envip");
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 
-	envp[(*envi)++] = g_strdup_printf("%s=%s",
+	task_data = g_object_get_data(scgi_task, "data");
+	task_data->envp[task_data->envi++] = g_strdup_printf("%s=%s",
 				key, value);
 }
 
@@ -179,23 +171,19 @@ static void req_input_stream_read_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data)
 {
 	GObject *scgi_task = user_data;
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 	gssize size = 0;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	task_data = g_object_get_data(scgi_task, "data");
 
 	size = g_input_stream_read_finish(G_INPUT_STREAM(source_object),
 				res, NULL);
 	if(0 <= size)
 	{
-		GOutputStream *child_output_stream = NULL;
-		guint *in_buffer = g_object_get_data(scgi_task,
-					"in_buffer");
-		
-		child_output_stream = g_object_get_data(scgi_task,
-					"child_output_stream");
-
-		g_output_stream_write_async(child_output_stream,
-					in_buffer, size, G_PRIORITY_DEFAULT, NULL,
+		g_output_stream_write_async(task_data->child_output_stream,
+					task_data->in_buffer, size, G_PRIORITY_DEFAULT, NULL,
 					child_output_stream_write_async_handler, scgi_task);
 	}
 	else
@@ -208,45 +196,37 @@ static void res_output_stream_write_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data)
 {
 	GObject *scgi_task = user_data;
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 	gssize size = 0;
-	GInputStream *child_input_stream = NULL;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
+	task_data = g_object_get_data(scgi_task, "data");
+
 	size = g_output_stream_write_finish(G_OUTPUT_STREAM(source_object),
 				res, NULL);
-
-	child_input_stream = g_object_get_data(scgi_task,
-				"child_input_stream");
-
 	if(0 < size)
 	{
-		gchar *out_buffer = NULL;
-		guint *out_count = NULL;
-		gboolean *is_checked = NULL;
-
-		out_buffer = g_object_get_data(scgi_task, "out_buffer");
-		out_count = g_object_get_data(scgi_task, "out_count");
-		is_checked = g_object_get_data(scgi_task, "is_checked");
-
-		if(FALSE == (*is_checked))
+		if(FALSE == task_data->is_checked)
 		{
 			g_output_stream_write_async(G_OUTPUT_STREAM(source_object),
-						out_buffer, *out_count, G_PRIORITY_DEFAULT, NULL,
+						task_data->out_buffer, task_data->out_count,
+						G_PRIORITY_DEFAULT, NULL,
 						res_output_stream_write_async_handler, scgi_task);
-			*is_checked = TRUE;
+			task_data->is_checked = TRUE;
 		}
 		else
 		{
-			*out_count = 0;
-			g_input_stream_read_async(child_input_stream, out_buffer,
-						BUFFER_SIZE, G_PRIORITY_DEFAULT, NULL,
+			task_data->out_count = 0;
+			g_input_stream_read_async(task_data->child_input_stream,
+						task_data->out_buffer, BUFFER_SIZE,
+						G_PRIORITY_DEFAULT, NULL,
 						child_input_stream_read_async_handler, scgi_task);
 		}
 	}
 	else if(0 >= size)
 	{
-		g_input_stream_close_async(child_input_stream,
+		g_input_stream_close_async(task_data->child_input_stream,
 					G_PRIORITY_DEFAULT, NULL, 
 					child_input_stream_close_async_handler,
 					scgi_task);
@@ -259,55 +239,46 @@ static void child_input_stream_read_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data)
 {
 	GObject *scgi_task = user_data;
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 	gssize size = 0;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	task_data = g_object_get_data(scgi_task, "data");
 
 	size = g_input_stream_read_finish(G_INPUT_STREAM(source_object),
 				res, NULL);
 	if(0 <= size)
 	{
-		gchar *out_buffer = NULL;
-		guint *out_count = NULL;
-
-		out_buffer = g_object_get_data(scgi_task, "out_buffer");
-		out_count = g_object_get_data(scgi_task, "out_count");
-
-		*out_count += size;
-		if((0==size) || (BUFFER_SIZE<=(*out_count)))
+		task_data->out_count += size;
+		if((0==size) || (BUFFER_SIZE<=task_data->out_count))
 		{
-			GObject *scgi_response = NULL;
-			GOutputStream *output_stream = NULL;
 			gchar *header_end = NULL;
-			gboolean *is_checked = NULL;
 
-			scgi_response = hev_scgi_task_get_response(HEV_SCGI_TASK(scgi_task));
-			output_stream = hev_scgi_response_get_output_stream(HEV_SCGI_RESPONSE(scgi_response));
-			is_checked = g_object_get_data(scgi_task, "is_checked");
-
-			if((FALSE==(*is_checked)) &&
-						(header_end=g_strstr_len(out_buffer,
-												 *out_count, "\r\n\r\n")) &&
-						(NULL==g_strstr_len(out_buffer, header_end-out_buffer,
-									  "Status:")))
+			if((FALSE==task_data->is_checked) &&
+					(header_end=g_strstr_len(task_data->out_buffer,
+											 task_data->out_count, "\r\n\r\n")) &&
+					(!g_strstr_len(task_data->out_buffer,
+								   header_end-task_data->out_buffer, "Status:")))
 			{
-				g_output_stream_write_async(output_stream,
+				g_output_stream_write_async(task_data->res_output_stream,
 							"Status: 200 OK\r\n", 16, G_PRIORITY_DEFAULT, NULL,
 							res_output_stream_write_async_handler, scgi_task);
 			}
 			else
 			{
-				*is_checked = TRUE;
-				g_output_stream_write_async(output_stream,
-							out_buffer, *out_count, G_PRIORITY_DEFAULT, NULL,
+				task_data->is_checked = TRUE;
+				g_output_stream_write_async(task_data->res_output_stream,
+							task_data->out_buffer, task_data->out_count,
+							G_PRIORITY_DEFAULT, NULL,
 							res_output_stream_write_async_handler, scgi_task);
 			}
 		}
 		else
 		{
 			g_input_stream_read_async(G_INPUT_STREAM(source_object),
-						out_buffer+(*out_count), BUFFER_SIZE-(*out_count),
-						G_PRIORITY_DEFAULT, NULL,
+						task_data->out_buffer+task_data->out_count,
+						BUFFER_SIZE-task_data->out_count, G_PRIORITY_DEFAULT, NULL,
 						child_input_stream_read_async_handler, scgi_task);
 		}
 
@@ -322,28 +293,22 @@ static void child_output_stream_write_async_handler(GObject *source_object,
 			GAsyncResult *res, gpointer user_data)
 {
 	GObject *scgi_task = user_data;
+	HevSCGIHandlerCGITaskData *task_data = NULL;
 	gssize size = 0;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
+	task_data = g_object_get_data(scgi_task, "data");
+
 	size = g_output_stream_write_finish(G_OUTPUT_STREAM(source_object),
 				res, NULL);
-
 	if(0 < size)
 	{
-		GObject *scgi_request = NULL;
-		GInputStream *input_stream = NULL;
-		guint *in_buffer = g_object_get_data(scgi_task, "in_buffer");
-		guint *in_count = g_object_get_data(scgi_task, "in_count");
-		guint *content_len = g_object_get_data(scgi_task, "content_len");
 		guint len = 0;
 
-		scgi_request = hev_scgi_task_get_request(HEV_SCGI_TASK(scgi_task));
-		input_stream = hev_scgi_request_get_input_stream(HEV_SCGI_REQUEST(scgi_request));
-
-		*in_count += size;
-		len = *content_len - *in_count;
-		g_input_stream_read_async(input_stream, in_buffer,
+		task_data->in_count += size;
+		len = task_data->content_len - task_data->in_count;
+		g_input_stream_read_async(task_data->req_input_stream, task_data->in_buffer,
 					(len<BUFFER_SIZE)?len:BUFFER_SIZE, G_PRIORITY_DEFAULT, NULL,
 					req_input_stream_read_async_handler,
 					scgi_task);
@@ -375,5 +340,16 @@ static void child_input_stream_close_async_handler(GObject *source_object,
 
 	g_input_stream_close_finish(G_INPUT_STREAM(source_object),
 				res, NULL);
+}
+
+static void hev_scgi_handler_cgi_task_data_destroy_handler(gpointer data)
+{
+	HevSCGIHandlerCGITaskData *task_data = data;
+
+	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	g_strfreev(task_data->envp);
+
+	g_slice_free(HevSCGIHandlerCGITaskData, task_data);
 }
 
